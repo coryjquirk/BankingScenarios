07b918fc5b927f34dcf33dcb747bc1a18de1e38e
Cory Quirk <coryjquirk@gmail.com>
2022-03-05 18:53:18 -0600

    stores balance in file1.txt upon withdraw/deposit

--- src/teksystems/java/Banking.java
+++ src/teksystems/java/Banking.java
@@ -3,34 +3,168 @@ import java.io.*;
 import java.util.Date;
 
 public class Banking {
-    String userName;
-    String userAddress;
-    String bankName;
-    String bankAddress;
-    long routingNumber;
-    long accountNumber;
-    //TODO: fix methods for deposit/withdraw
-    //current behavior:
-    //1. deposit 55
-    //2. current balance 55
-    //3. withdraw 44
-    //4. current balance -44
-    static double balance;
-    double annualFees;
-    float interestRate;
-    float tax;
-    Date dateCreated = new Date();
+    private String userName;
+    private String userAddress;
+    private String bankName;
+    private String bankAddress;
+    private long routingNumber;
+    private long accountNumber;
+    private double annualFees;
+    private float interestRate;
+    private float tax;
+    private Date dateCreated = new Date();
+    //TODO: use FileWriter to store balance
+    private static Double balance;
+    private static String filePath = "file1.txt";
+
+    public static void initBankdataFile(){
+        try {
+            File myObj = new File(filePath);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("Bank data file exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    public static String readFromFile() {
+        String fileString = null;
+        try {
+            FileReader myReader = new FileReader(filePath);
+            StringBuilder str = new StringBuilder();
+            int i;
+            while ((i = myReader.read()) != -1) {
+//                System.out.print((char) i);
+                str.append((char) i);
+            }
+            System.out.print("\n");
+            myReader.close();
+            System.out.println("Successfully read data from " + filePath);
+            fileString = str.toString();
+            return fileString;
+        } catch (IOException e) {
+            System.out.println("A file reading error occurred.");
+            e.printStackTrace();
+        }
+        return fileString;
+    }
+
+
+    public static void writeToFile(Double balance){
+        try {
+            FileWriter myWriter = new FileWriter(filePath);
+
+            String writeThis = balance.toString();
+
+            myWriter.write(writeThis);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("A file writing error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public float getTax() {
+        return tax;
+    }
+
+    public void setTax(float tax) {
+        this.tax = tax;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankAddress() {
+        return bankAddress;
+    }
+
+    public void setBankAddress(String bankAddress) {
+        this.bankAddress = bankAddress;
+    }
+
+    public long getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(long routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public static double getBalance() {
+        return balance;
+    }
+
+    public static void setBalance(double balance) {
+        Banking.balance = balance;
+        writeToFile(balance);
+    }
+
+    public double getAnnualFees() {
+        return annualFees;
+    }
+
+    public void setAnnualFees(double annualFees) {
+        this.annualFees = annualFees;
+    }
+
+    public float getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(float interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
 
     //TODO: create subclasses for account types
     // with specific methods for each class
-
-    public static double deposit(double depositAmt){
-        balance =+ depositAmt;
-        return balance;
+    public static void deposit(double depositAmt){
+        setBalance(balance+=depositAmt);
     }
-    public static double withdraw(double withdrawAmt){
-        balance =- withdrawAmt;
-        return balance;
+
+    public static void withdraw(double withdrawAmt){
+        setBalance(balance-=withdrawAmt);
     }
 
 }
--- src/teksystems/java/Runnable.java
+++ src/teksystems/java/Runnable.java
@@ -1,45 +1,54 @@
 package teksystems.java;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 //TODO: import Locale based currency formatter
 
 public class Runnable {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Banking.initBankdataFile();
+        Banking.setBalance(Double.parseDouble(Banking.readFromFile()));
+
         Scanner sc = new Scanner(System.in);
-        String userInterface = new String("Please select an option: \n1: Deposit to account \n2: Withdraw from account \n3: Show current balance \n4: Exit ");
+        String userInterface = "Please select an option: \n1: Deposit to account \n2: Withdraw from account \n3: Show current balance \n4: Exit ";
         System.out.print(userInterface);
         int depositOrWithdraw = sc.nextInt();
-        boolean switchAgain = false;
         do {
             switch (depositOrWithdraw) {
                 case 1:
                     System.out.println("Please enter deposit amount: ");
                     double depositAmt = sc.nextDouble();
-                    Banking.deposit(depositAmt);
-                    System.out.println("Balance: " + Banking.balance);
+                    if (depositAmt > 0){
+                        Banking.deposit(depositAmt);
+                        System.out.println("Balance: " + Banking.getBalance());
+                    } else {
+                        System.out.println("Deposit amount must be greater than 0.");
+                    }
                     System.out.println(userInterface);
-                    switchAgain = true;
                     depositOrWithdraw = sc.nextInt();
                     break;
                 case 2:
                     System.out.println("Please enter withdraw amount: ");
                     double withdrawAmt = sc.nextDouble();
-                    Banking.withdraw(withdrawAmt);
-                    System.out.println("Balance: " + Banking.balance);
+                    if (withdrawAmt > 0){
+                        Banking.withdraw(withdrawAmt);
+                        System.out.println("Balance: " + Banking.getBalance());
+                    } else {
+                        System.out.println("Withdraw amount must be greater than 0.");
+                    }
                     System.out.println(userInterface);
-                    switchAgain = true;
                     depositOrWithdraw = sc.nextInt();
                     break;
                 case 3:
-                    System.out.println("Balance: " + Banking.balance);
+                    System.out.println("Balance: " + Banking.getBalance());
                     System.out.println(userInterface);
-                    switchAgain = true;
                     depositOrWithdraw = sc.nextInt();
-                    break;
                 case 4:
+                    //TODO: weird bug for the Exit msg.
+                    System.out.println("Thank you! Exiting. Balance: " + Banking.getBalance());
                     break;
             }
-        } while (switchAgain = true);
-        System.out.println(Banking.balance);
+        } while(depositOrWithdraw != 4);
         sc.close();
     }
 }
